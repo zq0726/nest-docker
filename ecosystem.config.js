44149bf56e7f708189e0546a6b6f11c3aaa2c960
赵庆 <805322803@qq.com>
2025-05-14 16:31:57 +0800

    fix: 修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,6 @@ module.exports = {
     {
       name: 'nest_docker',
       script: 'dist/main.js',
-      // error_file: '/dev/stdout',
-      // out_file: '/dev/stdout',
-      // log_file: '/dev/stdout', // 强制所有日志输出到控制台
       autorestart: true, //是否自动重启
       instances: 2, //要启动实例的数量即负载数量
       error_file: '/app/log/nest-error.log',
