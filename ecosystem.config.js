be0b15b7a8f9869460022a53c3d10e25c407d3ec
赵庆 <805322803@qq.com>
2025-05-14 11:48:05 +0800

    fix: 修复 pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,9 @@ module.exports = {
     {
       name: 'nest_docker',
       script: 'dist/main.js',
-      out_file: '/app/log/file.log', //日志文件
-      error_file: '/app/log/file_error.log', //错误日志文件
+      error_file: '/dev/stdout',
+      out_file: '/dev/stdout',
+      log_file: '/dev/stdout', // 强制所有日志输出到控制台
       autorestart: true, //是否自动重启
       instances: 2, //要启动实例的数量即负载数量
     },
