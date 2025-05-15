e14e1e88fc965e214ad02706ad45a418c3519ab8
赵庆 <805322803@qq.com>
2025-05-15 15:02:50 +0800

    feat: 再次部署

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! hello qing';
+    return ' 真的有点问题啊 Hello World! hello qing';
   }
 }
