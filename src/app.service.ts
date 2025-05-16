d0e3410d85b87d8749521775aa882cc6b9c1d9b6
赵庆 <805322803@qq.com>
2025-05-16 16:02:59 +0800

    feat: test

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ import { CacheService } from './cache/cache.service';
 export class AppService {
   constructor(@Inject() private cacheService: CacheService) {}
   getHello(): string {
-    return '  Hello World! && cg  mysql  gg';
+    return '  Hello World! && cg  mysql  gg  44';
   }
 
   setCache(key: string, value: unknown, second: number = 1000): any {
