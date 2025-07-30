7161ea2908b0bfa7041cc1d2328289e7c6b0993a
赵庆 <805322803@qq.com>
2025-07-30 16:05:43 +0800

    feat: fwq build

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,6 @@ async function bootstrap() {
   const port = process.env.APP_PORT || 3000;
   await app.listen(port);
 
-  console.log(pc.yellow(`\n 服务器运行在：http://localhost:${port} `));
+  console.log(pc.yellow(`\n 服务器运行在：http://localhost:${port} 上`));
 }
 bootstrap();
